2288f2bdd250e3a385f3e318e7981de9d8d676a7
chadm9 <chadm_@yahoo.com>
2024-05-14 12:56:59 -0400

    read requests

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+import json
 import os
 from google.cloud import secretmanager
 import flask
 import functions_framework
 
 # Retrieve Job-defined env vars
-TASK_INDEX = os.getenv("CLOUD_RUN_TASK_INDEX", 0)
-TASK_ATTEMPT = os.getenv("CLOUD_RUN_TASK_ATTEMPT", 0)
 PROJECT_ID = os.getenv("PROJECT_ID", "neon-nexus-297211")
 FRAME_KEY_SECRET = os.getenv("FRAME_KEY_SECRET", "frame-key")
 FRAME_KEY_SECRET_VERSION = os.getenv("FRAME_KEY_SECRET_VERSION", 1)
@@ -13,12 +12,19 @@ FRAME_KEY_SECRET_VERSION = os.getenv("FRAME_KEY_SECRET_VERSION", 1)
 
 @functions_framework.http
 def hello(request: flask.Request) -> flask.typing.ResponseReturnValue:
-    print(request.data)
+    try:
+        request_body = json.loads(request.data.decode("UTF-8"))
+    except:
+        request_body = json.loads("{}")
+
+    print("Function Execution Started")
+    print(request_body)
 
     api_key = get_secret_manager_secret_version()
     # print(f"Secret Value: {secret}.")
 
-    return f"Secret Value: {api_key}."
+    # return f"Secret Value: {api_key}."
+    return json.dumps(request_body)
 
 
 def get_secret_manager_secret_version():
